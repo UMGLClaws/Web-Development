3050028c2cee298ff214553e67d2337f6bf90db2
UMGLClaws <rileydrk123@gmail.com>
2025-03-13 09:03:50 -0230

    V 0.12
    Reorganization of files and minor CSS changes and some formatting changes in HTML.

--- functionality.js
+++ functionality.js
@@ -1,4 +1,11 @@
 function darkMode() {
-    var element = document.body;
-    element.classList.toggle("dark-mode");
+    var main = document.body;
+    var buttonDiv = document.getElementsByName("MyDIV");
+    main.classList.toggle("dark-mode");
+    if (buttonDiv.innerHTML === "Dark Mode") {
+        buttonDiv.innerHTML = "Light Mode";
+    }
+    else {
+        buttonDiv.innerHTML = "Dark Mode";
+    }
 }
